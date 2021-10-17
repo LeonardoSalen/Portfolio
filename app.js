9d642e16cac45ff6ecb727dba56aaa0575bdf63b
LeonardoSalen <90048141+LeonardoSalen@users.noreply.github.com>
2021-10-17 19:07:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 0,
+        "value": 60,
         "density": {
           "enable": true,
           "value_area": 800
